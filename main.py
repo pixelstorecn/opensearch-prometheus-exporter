b784f0ff76bde168df0601ac5474c830a8123f1b
lill25 <lill25@lenovo.com>
2024-02-25 23:47:07 +0800

    fix data type NoneType

--- main.py
+++ main.py
@@ -7,19 +7,23 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 import json
+import requests as rq
 
 app = FastAPI()
 @app.get('/metrics', response_class=PlainTextResponse)
 def get_data():
-    data_str = '[{"index":"flash-openapi-prod-2024-02-16-14","shard":"0","prirep":"p","state":"STARTED","docs":"2924399","store":"3040","ip":"10.62.217.39","node":"node-hot-temp"},{"index":"flash-openapi-prod-2024-02-16-14","shard":"0","prirep":"r","state":"STARTED","docs":"2924399","store":"3041","ip":"10.62.217.40","node":"node-hot-06"},{"index":"flash-openapi-prod-2024-02-16-14","shard":"1","prirep":"r","state":"STARTED","docs":"2927042","store":"3042","ip":"10.62.217.39","node":"node-hot-temp"}]'
-    data_arr = json.loads(data_str)
+    es_addr = "http://xxxxx:9200/_cat/shards?format=json&bytes=mb"
+    data_str = rq.get(es_addr, auth=('xxxx', 'xxxx'))
+    data_arr = json.loads(data_str.content)
     REGISTRY = CollectorRegistry(auto_describe=False)
     opensearch_shards_docs = Gauge("opensearch_shards_docs", "OpenSearch Shards Docs Count", ["index", "shard", "prirep", "ip", "node"], registry=REGISTRY)
     opensearch_shards_size = Gauge("opensearch_shards_size", "OpenSearch Shards Size", ["index", "shard", "prirep", "ip", "node"], registry=REGISTRY)
     for item in data_arr:
-        opensearch_shards_docs.labels(item['index'],item['shard'],item['prirep'],item['ip'],item['node']).set(item['docs'])
+        if item['docs'] != None:
+            opensearch_shards_docs.labels(item['index'],item['shard'],item['prirep'],item['ip'],item['node']).set(item['docs'])
     for item in data_arr:
-        opensearch_shards_size.labels(item['index'],item['shard'],item['prirep'],item['ip'],item['node']).set(item['store'])
+        if item['store'] != None:
+            opensearch_shards_size.labels(item['index'],item['shard'],item['prirep'],item['ip'],item['node']).set(item['store'])
     return prometheus_client.generate_latest(REGISTRY)
 
 if __name__ == "__main__":
