c6b702253befde59b898a3ee63bac75bce9a307a
artemus717 <artemus717@gmail.com>
2024-03-27 16:20:12 +0800

    fix format

--- main.py
+++ main.py
@@ -26,32 +26,32 @@ def get_data():
     opensearch_shards_size = Gauge("opensearch_shards_size", "OpenSearch Shards Size", ["index", "shard", "prirep", "ip", "node", "shortname"], registry=REGISTRY)
     for item in data_arr:
         pattern_hour = r"^([a-zA-Z-]*)-([0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2})$"
-	    pattern_day = r"^([a-zA-Z-_]*)-([0-9]{4}-[0-9]{2}-[0-9]{2})$"
+        pattern_day = r"^([a-zA-Z-_]*)-([0-9]{4}-[0-9]{2}-[0-9]{2})$"
         pattern_month = r"^([a-zA-Z-]*)-([0-9]{4}-[0-9]{2})$"
         if item['docs'] != None:
-		    item_index = item['index']
-			if re.search(pattern_hour, item_index):
-			    shortname = item_index[:-14]
-			elif re.search(pattern_day, item_index):
-			    shortname = item_index[:-11]
-			elif re.search(pattern_month, item_index):
-			    shortname = item_index[:-8]
-			else:
+            item_index = item['index']
+            if re.search(pattern_hour, item_index):
+                shortname = item_index[:-14]
+            elif re.search(pattern_day, item_index):
+                shortname = item_index[:-11]
+            elif re.search(pattern_month, item_index):
+                shortname = item_index[:-8]
+            else:
                 shortname = item_index
             opensearch_shards_docs.labels(item['index'],item['shard'],item['prirep'],item['ip'],item['node'],shortname).set(item['docs'])
     for item in data_arr:
         pattern_hour = r"^([a-zA-Z-]*)-([0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2})$"
-	    pattern_day = r"^([a-zA-Z-_]*)-([0-9]{4}-[0-9]{2}-[0-9]{2})$"
+        pattern_day = r"^([a-zA-Z-_]*)-([0-9]{4}-[0-9]{2}-[0-9]{2})$"
         pattern_month = r"^([a-zA-Z-]*)-([0-9]{4}-[0-9]{2})$"
         if item['store'] != None:
             item_index = item['index']
             if re.search(pattern_hour, item_index):
-			    shortname = item_index[:-14]
-			elif re.search(pattern_day, item_index):
-			    shortname = item_index[:-11]
-			elif re.search(pattern_month, item_index):
-			    shortname = item_index[:-8]
-			else:
+                shortname = item_index[:-14]
+            elif re.search(pattern_day, item_index):
+                shortname = item_index[:-11]
+            elif re.search(pattern_month, item_index):
+                shortname = item_index[:-8]
+            else:
                 shortname = item_index
             opensearch_shards_size.labels(item['index'],item['shard'],item['prirep'],item['ip'],item['node'],shortname).set(item['store'])
     return prometheus_client.generate_latest(REGISTRY)
